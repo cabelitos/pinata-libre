76b26a56c0e1ac0df3c6687f30108ce1d984b13f
Guilherme Iscaro <cabelitostos@gmail.com>
2020-12-23 01:06:36 -0300

    feat: add missing scope for reading user profile

--- src/index.ts
+++ src/index.ts
@@ -65,7 +65,12 @@ const startRtmService = async (): Promise<void> => {
   app.get('/install', async (_, res) => {
     try {
       const url = await installerProvider.generateInstallUrl({
-        scopes: ['app_mentions:read', 'channels:history', 'chat:write'],
+        scopes: [
+          'app_mentions:read',
+          'channels:history',
+          'chat:write',
+          'users.profile:read',
+        ],
       });
       res.send(
         `<a href=${url}><img alt=""Add to Slack"" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>`,
